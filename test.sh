3178967914d4d44af821ee65ea85ad6dad85cc56
Daniel Nicorici <Daniel.Nicorici@gmail.com>
2021-01-12 14:26:59 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,12 @@
 make clean
 make
 
-wget https://sourceforge.net/projects/fusioncatcher/files/test/reads_1.fq.gz -O reads_1.fq.gz
-wget https://sourceforge.net/projects/fusioncatcher/files/test/reads_2.fq.gz -O reads_2.fq.gz
-
-gzip -f -d *.gz
+FILE=reads_1.fq
+if [ ! -f "$FILE" ]; then
+    wget https://sourceforge.net/projects/fusioncatcher/files/test/reads_1.fq.gz -O reads_1.fq.gz
+    wget https://sourceforge.net/projects/fusioncatcher/files/test/reads_2.fq.gz -O reads_2.fq.gz
+    gzip -f -d *.gz
+fi
 
 # test 1
 echo "Test 1"
@@ -17,9 +19,9 @@ diff r2.fq reads_2.fq
 
 # test 1
 echo "Test 2"
-./fastq-leave interleave reads_1.fq reads_2.fq - | ./fastq-leave deinterleave - r1.fq r2.fq
-diff r1.fq reads_1.fq
-diff r2.fq reads_2.fq
+./fastq-leave interleave reads_1.fq reads_2.fq - | ./fastq-leave deinterleave - r1_.fq r2_.fq
+diff r1_.fq reads_1.fq
+diff r2_.fq reads_2.fq
 
 
 echo "Test 3"
@@ -50,3 +52,53 @@ echo "Test 10"
 echo "Test 11"
 ./fastq-leave detab tab8_ri.txt ri_8back.fq
 
+echo "Test 12"
+./fastq-leave drop-short 101 reads_1.fq r1_dropped.fq
+cat reads_1.fq | ./fastq-leave drop-short 101 - - | cat >r1_dropped_.fq
+
+echo "Test 13"
+./fastq-leave NtoA reads_1.fq reads_1_n2a.fq
+#sed 's/GAAAAAA/GNNNNNN/g' reads_1.fq | ./fastq-leave  NtoA - reads_1_n2a.fq
+#diff reads_1_n2a.fq reads_1.fq
+
+echo "Test 14"
+./fastq-leave trim5 7 reads_1.fq reads_1_trim5_7.fq
+./fastq-leave trim5 99 reads_1.fq reads_1_trim5_99.fq
+./fastq-leave trim5 0 reads_1.fq reads_1_trim5_0.fq
+
+echo "Test 15"
+./fastq-leave trim3 7 reads_1.fq reads_1_trim3_7.fq
+./fastq-leave trim3 99 reads_1.fq reads_1_trim3_99.fq
+./fastq-leave trim3 0 reads_1.fq reads_1_trim3_0.fq
+
+echo "Test 16"
+./fastq-leave retain5 7 reads_1.fq reads_1_retain5_7.fq
+./fastq-leave retain5 99 reads_1.fq reads_1_retain5_99.fq
+./fastq-leave retain5 100 reads_1.fq reads_1_retain5_100.fq
+
+echo "Test 17"
+./fastq-leave retain3 7 reads_1.fq reads_1_retain3_7.fq
+./fastq-leave retain3 99 reads_1.fq reads_1_retain3_99.fq
+./fastq-leave retain3 100 reads_1.fq reads_1_retain3_100.fq
+
+echo "Test 18"
+./fastq-leave trim-id reads_1.fq reads_1_trim-id.fq
+./fastq-leave trim-id reads_2.fq reads_2_trim-id.fq
+
+echo "Test 19"
+./fastq-leave trim-N reads_1.fq reads_1_trimN.fq
+./fastq-leave trim-N reads_2.fq reads_2_trimN.fq
+
+echo "Test 20"
+./fastq-leave trim-polyA 12 reads_1.fq reads_1_trim-polya.fq
+./fastq-leave trim-polyA 12 reads_2.fq reads_2_trim-polya.fq
+
+echo "Test 21"
+./fastq-leave compress-id @ count_reads_1.txt reads_1.fq reads_1_compressed-ids.fq
+./fastq-leave compress-id /1 count_reads_1.txt reads_1.fq reads_1_compressed-ids-1.fq
+./fastq-leave compress-id /2 count_reads_1.txt reads_2.fq reads_2_compressed-ids-2.fq
+./fastq-leave compress-id _1 count_reads_1.txt reads_1.fq reads_1_compressed-ids-1_.fq
+./fastq-leave compress-id _2 count_reads_1.txt reads_2.fq reads_2_compressed-ids-2_.fq
+./fastq-leave compress-id @@ count_ri.txt ri.fq ri_compressed-ids.fq
+./fastq-leave compress-id /12 count_ri.txt ri.fq ri_compressed-ids-12.fq
+./fastq-leave compress-id _12 count_reads_1.txt ri.fq ri_compressed-ids_12.fq
